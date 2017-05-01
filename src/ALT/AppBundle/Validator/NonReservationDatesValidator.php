2e7be41270385120f076ac836ab5dcd9a6196b52
ALT <alts.thomas@gmail.com>
2017-05-01 18:05:41 +0200

    Modification des messages pour les dates de non réservation

--- src/ALT/AppBundle/Validator/NonReservationDatesValidator.php
+++ src/ALT/AppBundle/Validator/NonReservationDatesValidator.php
@@ -23,14 +23,14 @@ class NonReservationDatesValidator extends ConstraintValidator
 
         $jourmois = $value->format('d-m');
         if (in_array($jourmois, self::getJoursFermes())) {
-            $this->context->addViolation($constraint->message);
+            $this->context->addViolation('Le Musée du Louvre est fermé les 1er mai, les 11 novembre et les 25 decembre, ');
         }
 
         // vérification dimanche & mardi
         // savoir si le numéro du jour dans la semaine vaut (7 = dimanche, 2 mardi)
         $jourDeLaSemaine = $value->format('N');
         if ($jourDeLaSemaine == 7 || $jourDeLaSemaine == 2) {
-            $this->context->addViolation($constraint->message);
+            $this->context->addViolation('Le Musée du Louvre est fermé les mardi et dimanche');
         }
 
         // si la journée est passée
@@ -39,7 +39,7 @@ class NonReservationDatesValidator extends ConstraintValidator
         $now->setTime(0,0,0);
 
         if ($value < $now) {
-            $this->context->addViolation($constraint->message);
+            $this->context->addViolation("Il n'est pas possible de commander pour une date antérieur à celle du jour");
         }
 
 
